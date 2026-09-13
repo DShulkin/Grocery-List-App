7766e58e89973f8144abc67ac5f41352c5ac189e
David Shulkin <thedavidshulkin@gmail.com>
2026-09-13 16:39:00 -0700

    updating state

--- src/main.jsx
+++ src/main.jsx
@@ -43,14 +43,23 @@ const Item = (props) => {
   )
 }
 
-const Counter = (props) => {
-const [quantity, setSQuantity] = useState(0)
+const Counter = () => {
+  const [quantity, setQuantity] = useState(0)
+  const incrementQuantity = () => {
+    setQuantity(quantity + 1)
+  }
+
+  const decrementQuantity = () => {
+    if (quantity > 0) {
+      setQuantity(quantity - 1)
+    }
+  }
 
   return (
     <div className="quantity">
       <span className="qty-label">QTY</span>
-      <button className="increment">+</button>
-      <button className="decrement">-</button>
+      <button className="increment" onClick={incrementQuantity}>+</button>
+      <button className="decrement"onClick={decrementQuantity}>-</button>
       <span className="quantity-amount">{quantity}</span>
     </div>
   )
