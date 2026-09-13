0391421ff0f2c7a6ee3d9f4fb821ce2b7feadc7d
David Shulkin <thedavidshulkin@gmail.com>
2026-09-13 16:26:04 -0700

    adding state to a component

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,27 @@
 import { createRoot } from 'react-dom/client'
-import { StrictMode} from 'react' 
+import { StrictMode, useState } from 'react' 
 
 const items = [
   {
     name: "Apples",
-    quantity: 5
+    quantity: 5,
+    id : 1
   },
   {
     name: "Bananas",
-    quantity: 7
+    quantity: 7,
+    id : 2
   },
   {
     name: "Box of Pasta",
-    quantity: 1
+    quantity: 1,
+    id : 3
   },
   {
     name: "Cookies",
-    quantity: 12
-  }
+    quantity: 12,
+    id : 4
+  },
 ]
 
 const Header = (props) => {
@@ -34,22 +38,23 @@ const Item = (props) => {
     <div className="item">
       <button className="remove-item" />
       <span className="item-name">{props.name}</span>
-      <Counter quantity={props.quantity} />
+      <Counter />
     </div>
   )
 }
 
 const Counter = (props) => {
+const [quantity, setSQuantity] = useState(0)
+
   return (
     <div className="quantity">
       <span className="qty-label">QTY</span>
       <button className="increment">+</button>
       <button className="decrement">-</button>
-      <span className="quantity-amount">{props.quantity}</span>
+      <span className="quantity-amount">{quantity}</span>
     </div>
   )
 }
-  
 
 const App = (props) => {
   return (
@@ -63,7 +68,8 @@ const App = (props) => {
       {props.initialList.map( item => 
         <Item 
           name={item.name} 
-          quantity={item.quantity} />
+          key={item.id}  
+        />
       )}
     </div>
   )
